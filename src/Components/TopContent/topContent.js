40239f2ec99b6ce4385a667f2de86b93e476c852
Selva81294 <selva81294@gmail.com>
2023-03-15 12:04:11 +0530

    commit

--- src/Components/TopContent/topContent.js
+++ src/Components/TopContent/topContent.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-scroll'
 import './topContent.css'
+import Resume from '../../Resume/My_Resume.pdf'
 
 const TopContent = () => {
   return (
@@ -8,7 +9,7 @@ const TopContent = () => {
         <div className='topContent_container'>
             <h1>Mr. Selva Kumaran</h1> 
             <p>A Full Stack Web & App Developer</p>
-            <a href='www.google.com'>
+            <a href= {Resume} download>
                 <button className='topContent_downloadBtn'>Download CV</button>
             </a>
             <Link to="projects" smooth={true} duration={500}>
